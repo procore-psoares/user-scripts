968e7aef74a29d5c95743a7e2f68e7e55dfe6d54
Pablo Soares <pablo.soares-contractor@procore.com>
2022-01-24 14:36:19 -0300

    Add values tab auto-scroll. Refactor

--- tugboat-deployment-prioritizer.js
+++ tugboat-deployment-prioritizer.js
@@ -1,67 +1,80 @@
 // ==UserScript==
 // @name     tugboat-prioritzer
-// @version  2
-// @include  https://tugboat.procorecon.com/* https://tugboat.procoretech-qa.com/*
+// @version  3
+// @include  https://tugboat.procoretech-qa.com/*
 // @grant    none
 // ==/UserScript==
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function relocateProcoreWeb(tbody) {
-    console.debug('relocateProcoreWeb')
+  console.debug('relocateProcoreWeb')
 
-    list = tbody.querySelectorAll('tr')
-    console.debug('list', list)
+  list = tbody.querySelectorAll('tr')
 
-    listArr = Array.from(list)
-    console.debug('listArr', listArr)
+  listArr = Array.from(list)
 
-    procoreWebDeplNode = listArr.find(x => x.textContent.includes("pz01-procore-web"))
-    console.debug('procoreWebDeplNode', procoreWebDeplNode)
+  procoreWebDeplNode = listArr.find(x => x.textContent.includes("pz01-procore-web"))
 
-    procoreWebDeplNode.style.border = '2px solid #f47e42'
-    tbody.insertBefore(procoreWebDeplNode, list[0])
-    console.debug('end')
+  procoreWebDeplNode.style.border = '2px solid #f47e42'
+  tbody.insertBefore(procoreWebDeplNode, list[0])
 }
 
 function watchForTableChanges() {
-    console.debug('watchForTableChanges')
-    const targetNode = document.querySelector('.layout--content')
-    const observerOptions = {
-        childList: true,
-        subtree: true
+  console.debug('watchForTableChanges')
+  const targetNode = document.querySelector('.layout--content')
+  const observerOptions = {
+    childList: true,
+    subtree: true
+  }
+
+  function callback(mutationList, observer) {
+    relocateProcoreWeb(mutationList[2].addedNodes[0].querySelector('tbody.core-table__body'))
+  }
+
+  const observer = new MutationObserver(callback)
+  observer.observe(targetNode, observerOptions)
+}
+
+let shouldAttemptToWatchValuesTab = true
+
+async function watchValuesTab() {
+  console.debug('watchValuesTab')
+
+  while (true) {
+    if (shouldAttemptToWatchValuesTab) {
+
+      const list = Array.from(document.querySelectorAll('div.ace_line_group'))
+      const procoreNode = list.find(x => x.innerText.includes('"procore": {'))
+
+      if (procoreNode != null) {
+        procoreNode.scrollIntoView({behavior: "smooth"})
+        shouldAttemptToWatchValuesTab = false
+      }
+
+      await sleep(1000)
+    } else {
+      await sleep(1000)
     }
-
-    function callback(mutationList, observer) {
-        console.debug('mutation callback')
-        console.debug('mutation list', mutationList)
-        console.debug('mutation list target', mutationList[2].addedNodes[0])
-        console.debug('mutation list target', mutationList[2].addedNodes[0].querySelector('tbody.core-table__body'))
-
-        relocateProcoreWeb(mutationList[2].addedNodes[0].querySelector('tbody.core-table__body'))
-    }
-
-    const observer = new MutationObserver(callback)
-    observer.observe(targetNode, observerOptions)
+    await sleep(1000)
+  }
 }
 
 async function waitForPageLoad() {
-    console.debug('waitForPageLoad')
-    await sleep(1000)
+  console.debug('waitForPageLoad')
+  await sleep(1000)
+  tbody = document.querySelector('tbody.core-table__body')
+
+  while (tbody == null) {
     tbody = document.querySelector('tbody.core-table__body')
+    await sleep(300)
+  }
 
-    while (tbody == null) {
-        console.debug('waiting')
-        tbody = document.querySelector('tbody.core-table__body')
-        await sleep(300)
-    }
-
-    console.debug('execute')
-    watchForTableChanges(tbody)
-    relocateProcoreWeb(tbody)
+  watchForTableChanges(tbody)
+  relocateProcoreWeb(tbody)
 }
 
-
 waitForPageLoad()
+watchValuesTab()
